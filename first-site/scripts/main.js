5cbe56d17ddbd8fbfa4df4ee273d8f1189f7c71a
cybzzz <454069895@qq.com>
2020-03-20 18:59:40 +0800

    3.20-3

--- first-site/scripts/main.js
+++ first-site/scripts/main.js
@@ -3,7 +3,7 @@ let myImage = document.querySelector('img');
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
     if(mySrc === 'images/headpic.jpg') {
-      myImage.setAttribute('src', 'images/2.png');
+      myImage.setAttribute('src', 'images/2.jpg');
     } else {
       myImage.setAttribute('src', 'images/headpic.jpg');
     }
